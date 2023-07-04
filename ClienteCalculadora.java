e03f4b2eca438f8605fa2d14729fc129bbcdd8e2
mpirixan <matheus.pirixan@techlead.com.br>
2023-07-04 18:27:46 -0300

    Adição de tela e multipc em simultaneo

--- ClienteCalculadora.java
+++ ClienteCalculadora.java
@@ -1,27 +1,33 @@
+import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class ClienteCalculadora {
   public static void main(String[] args) {
     try {
       // Localiza o registry. É possível usar endereço/IP porta
-      Registry registry = LocateRegistry.getRegistry(null);
+      //Registry registry = LocateRegistry.getRegistry("192.168.1.5",5080);
       // Consulta o registry e obtém o stub para o objeto remoto
-      Calculadora calc = (Calculadora) registry.lookup("calculadora");
+      String url = "rmi://localhost:5080/calculadora";
+      Calculadora calc = (Calculadora) Naming.lookup(url);
 
       // A partir deste momento, cahamadas à Caluladora podem ser
       // feitas como qualquer chamada a métodos
 
-      Numero num1 = new NumeroImpl(2);
-      Numero num2 = new NumeroImpl(1);
-      Numero num3 = new NumeroImpl(2.56);
-      Numero num4 = new NumeroImpl(1.86);
-      List<Numero> nums = Arrays.asList(num1, num2, num3, num4);
+      //Numero num1 = new NumeroImpl(2);
+     // Numero num2 = new NumeroImpl(1);
+     // Numero num3 = new NumeroImpl(2.56);
+     // Numero num4 = new NumeroImpl(1.86);
+     // List<Numero> nums = Arrays.asList(num1, num2, num3, num4);
       Double indice = 2.0;
+      double valor1, valor2;
 
       //Aqui são feitas diversas chamadas remotas
+      /*
       Numero soma = calc.somar(num1, num2);
       Numero sub = calc.subtrair(num1, num2);
       Numero mult = calc.multiplicar(num1, num2);
@@ -43,8 +49,127 @@ public class ClienteCalculadora {
       Numero horaSegundo = calc.converterHorasEmSegundos(new NumeroImpl(2));
       Numero segundoHora = calc.converterSegundosEmHoras(new NumeroImpl(7200));
       Numero celsiusFahrenheit = calc.converterCelsiusParaFahrenheit(new NumeroImpl(25));
-      Numero kgEmG = calc.converterKgParaGramas(new NumeroImpl(2.5)); 
+      Numero kgEmG = calc.converterKgParaGramas(new NumeroImpl(2.5));
 
+       */
+      //Leitura de dados e tela de comandos
+      Scanner ler = new Scanner(System.in);
+      int n;
+      int operacao;
+
+      TelaConsole telaConsole = new TelaConsole();
+      String limpaTela = "\033\143";
+      boolean program = true;
+      while (program ){
+        System.out.print(limpaTela);
+        telaConsole.tela();
+        operacao = ler.nextInt();
+        switch (operacao){
+          case 1:
+            System.out.print(limpaTela);
+            System.out.println("Número 1: ");
+            valor1 = ler.nextDouble();
+            System.out.println("+ : ");
+            valor2 = ler.nextDouble();
+            Numero soma = calc.somar(new NumeroImpl(valor1), new NumeroImpl(valor2));
+            System.out.println("Soma: " + soma.getValor());
+            System.out.println("Aperte 1 para continuar");
+            ler.next();
+            break;
+          case 2:
+            System.out.print(limpaTela);
+            System.out.println("Número 1: ");
+            valor1 = ler.nextDouble();
+            System.out.println("- : ");
+            valor2 = ler.nextDouble();
+            Numero subtracao = calc.somar(new NumeroImpl(valor1), new NumeroImpl(valor2));
+            System.out.println("Subtração: " + subtracao.getValor());
+            System.out.println("Aperte 1 para continuar");
+            ler.next();
+            break;
+          case 3:
+            System.out.print(limpaTela);
+            System.out.println("Número 1: ");
+            valor1 = ler.nextDouble();
+            System.out.println("X : ");
+            valor2 = ler.nextDouble();
+            Numero multiplicar = calc.multiplicar(new NumeroImpl(valor1), new NumeroImpl(valor2));
+            System.out.println("Multiplicação: " + multiplicar);
+            System.out.println("Aperte 1 para continuar");
+            ler.next();
+            break;
+          case 4:
+            System.out.print(limpaTela);
+            System.out.println("Número 1: ");
+            valor1 = ler.nextDouble();
+            System.out.println("/ : ");
+            valor2 = ler.nextDouble();
+            try {
+              Numero dividir = calc.dividir(new NumeroImpl(valor1), new NumeroImpl(valor2));
+              System.out.println("Dividir: " + dividir);
+            } catch (DivisaoPorZeroException e) {
+              System.out.println("Tentou dividir por zero! Esta é uma exceção remota.");
+            }
+            break;
+          case 5:
+            System.out.print(limpaTela);
+            System.out.println("Número 1: ");
+            valor1 = ler.nextDouble();
+            System.out.println("Expoente : ");
+            valor2 = ler.nextDouble();
+            Numero potencia = calc.potenciar(new NumeroImpl(valor1), new NumeroImpl(valor2));
+            System.out.println("Potencia: " + potencia.getValor());
+            System.out.println("Aperte 1 para continuar");
+            ler.next();
+            break;
+          case 6:
+            System.out.print(limpaTela);
+            System.out.println("Minuendo: ");
+            valor1 = ler.nextDouble();
+            System.out.println("Subtraendo : ");
+            valor2 = ler.nextDouble();
+            Numero absoluto = calc.absolutoDaDiferenca(new NumeroImpl(valor1), new NumeroImpl(valor2));
+            System.out.println("Absoluto: " + absoluto.getValor());
+            System.out.println("Aperte 1 para continuar");
+            ler.next();
+            break;
+          case 7:
+            System.out.print(limpaTela);
+            System.out.println("Número para arredondar acima: ");
+            valor1 = ler.nextDouble();
+            Numero arredondarAcima = calc.arredondarAcima(new NumeroImpl(valor1));
+            System.out.println("Acima: " + arredondarAcima.getValor());
+            System.out.println("Aperte 1 para continuar");
+            ler.next();
+            break;
+          case 8:
+            System.out.print(limpaTela);
+            System.out.println("Número para arredondar abaixo: ");
+            valor1 = ler.nextDouble();
+            Numero arredondarAbaixo = calc.arredondarAbaixo(new NumeroImpl(valor1));
+            System.out.println("Abaixo: " + arredondarAbaixo.getValor());
+            System.out.println("Aperte 1 para continuar");
+            ler.next();
+            break;
+          case 9:
+            System.out.print(limpaTela);
+            System.out.println("Número para arredondar acima: ");
+            int numFac = ler.nextInt();
+            Numero fatorial = calc.fatorial(numFac);
+            System.out.println("Acima: " + fatorial.getValor());
+            System.out.println("Aperte 1 para continuar");
+            ler.next();
+            break;
+          case 0:
+            program = false;
+            System.out.println("Programa encerrado.");
+            break;
+          default:
+            System.out.println("Opção inválida. Tente novamente.");
+            break;
+        }
+      }
+/*
       System.out.println("Resultados obtidos do servidor:" +
                         "\n\t+:" + soma.getValor() +
                         "\n\t-:" + sub.getValor()  +
@@ -58,7 +183,7 @@ public class ClienteCalculadora {
                         "\n\tminimo:" + minimo.getValor() +
                         "\n\tmaximo:" + maximo.getValor() +
                         "\n\tmedia:" + media.getValor() +
-                        "\n\tabsoluto da diferença:" + absolutoDaDiferenca.getValor() + 
+                        "\n\tabsoluto da diferença:" + absolutoDaDiferenca.getValor() +
                         "\n\tarredondar para inteiro:" + arredondar1.getValor() + 
                         "\n\tarredondar para inteiro:" + arredondar2.getValor() +
                         "\n\tarredondar para inteiro:" + arredondar3.getValor() +
@@ -70,14 +195,19 @@ public class ClienteCalculadora {
                         "\n\t2.5 kg em gramas: " + kgEmG.getValor()
                       );
 
+
+
       try {
         calc.dividir(new NumeroImpl(1), new NumeroImpl(0));
       } catch (DivisaoPorZeroException e) {
         System.out.println("Tentou dividir por zero! Esta é uma exceção remota.");
       }
 
+
+ */
     } catch (Exception e) {
       System.err.println("Ocorreu um erro no cliente: " + e.toString());
     }
   }
+
 }
---
+++ TelaConsole.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class TelaConsole {
+    public void tela(){
+        System.out.println("Calculadora\n" +
+                "Escolha a opcao de calculo\n" +
+                "1 - Soma: \n" +
+                "2 - Subtracao: \n" +
+                "3 - Multiplicacao: \n" +
+                "4 - Divisao: \n" +
+                "5 - Potencia: \n" +
+                "6 - Absoluto: \n" +
+                "7 - Acima: \n" +
+                "8 - Abaixo: \n" +
+                "9 - Fatorial: \n" +
+                "10 - Minimo: \n" +
+                "11 - Maximo: \n" +
+                "12 - Media: \n" +
+                "13 - Absoluto da Diferenca: \n" +
+                "14 - Arredondar para Inteiro 1: \n" +
+                "15 - Arredondar para Inteiro 2: \n" +
+                "16 - Arredondar para Inteiro 3: \n" +
+                "17 - Raiz Quadrada: \n" +
+                "18 - Radiciacao: \n" +
+                "19 - 2 horas em segundos: \n" +
+                "20 - 7200 segundos em horas: \n" +
+                "21 - 25 graus Celsius em Fahrenheit: \n" +
+                "22 - 2.5 kg em gramas: \n" +
+                "0 - Sair do Cliente");
+
+    }
+}
